a2b00d06243a8d49a3bfacc3657947f938bd189a
Nathan Macharia <Natha Macharia115@gmail.com>
2024-05-19 23:30:59 +0300

    hopefullyfinal commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,9 +17,9 @@ export async function fetchRevenue() {
    noStore()
   try {
     // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+    // Don't do this in production :)S
 
-     console.log('Fetching revenue data...');
+    console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
